aa740f4a1918bde501583cccedb4077b854e097b
Heriberto Delgado <heribertod@gmail.com>
2020-01-28 06:26:14 -0600

    Added support for partial runs, a few new error mesages, and a more comprehensive help text. Redesigned input argument structure for the program.

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 Convert very large decimal numbers into binary numbers.
 
-Usage: dec2bin path/to/input/number.txt path/to/binary/number.bin [offset into input file] [amount of bytes to read from input file]
+Usage: dec2bin [arguments]
+
+Arguments:
+ -i : Path to input decimal number file
+ -o : Path to output binary number file
+ -s : Offset into input file (starting index)
+ -l : Amount of digits to read from file (length)
+ -t : Minimum amount of time that dec2bin will run (hh:mm:ss)
+ -c : Path to cache file storing results for a partial run
+
+The input is expected to be a continuous stream of ASCII-encoded numeric digits (0x30 to 0x39). The output is a single binary blob representing the final conversion, in 8-bit bytes, MSB first. If the resulting number does not fit in a byte boundary, the result will be padded to the left with up to seven 0 bits.
+
+For a single run - that is, when you run dec2bin exactly once with a small file, 10MB or less - be sure to specify -i and -o, the minimum required arguments:
+
+    dec2bin -i pi-million.txt -o pi-million.bin
+
+If you need to extract only a section of a file, use -s and -l to specify the starting point and the length of that section:
+
+    dec2bin -i pi-billion.txt -o pi-billion.bin -s 2 -l 1000000
+
+If the amount of digits you need to convert exceed 10MB, while the program can still process them in a single run, the amount of time required might be too large - it's been estimated that the program needs about 9 months continuously running to convert a 1GB file, running in a 8-core Intel Core i9, 3.6MHz. In that case, we recommend you to use -t and -c to perform successive, controlled partial runs. This way, if dec2bin gets interrupted in one partial run, you only need to respecify the arguments, and the program will resume from the last partial run until time runs out, or the number is completed:
+
+    dec2bin -i pi-billion.txt -o pi-billion.bin -s 2 -l 1000000000 -t 8:00:00 -c pi-billion_cache.bin 
+
+Partial runs must be manually started. If you specify -t, dec2bin will only run for that amount of time, and stop immediately. You are responsible to continue running dec2bin with the required arguments until the conversion is completed.
 
 Source code is standard C++. Packaged inside an Xcode console app project; however, it can be compiled for any platform that supports minimum, basic C++11 features. 
--- dec2bin/main.cpp
+++ dec2bin/main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <chrono>
 #include <cmath>
+#include <sstream>
 
 using namespace std;
 
@@ -12,112 +13,314 @@ int main(int argc, const char * argv[])
 {
     try
     {
-        if (argc < 3 || argc > 5)
+        string input_file_name;
+        string output_file_name;
+        size_t input_start = 0;
+        size_t input_length = 0;
+        size_t seconds = 0;
+        string cache_file_name;
+        auto show_help = (argc == 1);
+        for (auto i = 1; i < argc; i++)
         {
-            cout << "Usage: dec2bin path/to/input/number.txt path/to/output/binary/number.bin [numeric offset into input file] [amount of bytes to read into file]" << endl;
+            string argument(argv[i]);
+            if (argument == "-i")
+            {
+                i++;
+                if (i >= argc)
+                {
+                    throw runtime_error("Input file name not specified");
+                }
+                input_file_name = argv[i];
+            }
+            else if (argument == "-o")
+            {
+                i++;
+                if (i >= argc)
+                {
+                    throw runtime_error("Output file name not specified");
+                }
+                output_file_name = argv[i];
+            }
+            else if (argument == "-s")
+            {
+                i++;
+                if (i >= argc)
+                {
+                    throw runtime_error("Offset not specified");
+                }
+                stringstream stream(argv[i], ios_base::in);
+                stream >> input_start;
+                if (stream.fail())
+                {
+                    throw runtime_error("Offset " + string(argv[i]) + " is not a positive number");
+                }
+            }
+            else if (argument == "-l")
+            {
+                i++;
+                if (i >= argc)
+                {
+                    throw runtime_error("Amount not specified");
+                }
+                stringstream stream(argv[i], ios_base::in);
+                stream >> input_length;
+                if (stream.fail())
+                {
+                    throw runtime_error("Amount " + string(argv[i]) + " is not a positive number");
+                }
+            }
+            else if (argument == "-t")
+            {
+                i++;
+                if (i >= argc)
+                {
+                    throw runtime_error("Time not specified");
+                }
+                string time = argv[i];
+                vector<size_t> components;
+                components.emplace_back();
+                for (size_t j = 0; j < time.length(); j++)
+                {
+                    auto c = time[j];
+                    if (c >= '0' && c <= '9')
+                    {
+                        components[components.size() - 1] = components[components.size() - 1] * 10 + (c - '0');
+                    }
+                    else if (c == ':')
+                    {
+                        if (components.size() >= 3)
+                        {
+                            throw runtime_error("Too many components present in time expression");
+                        }
+                        components.emplace_back();
+                    }
+                    else
+                    {
+                        throw runtime_error("Character '" + string(1, c) + "' unexpected in time expression");
+                    }
+                }
+                if (components.size() == 3)
+                {
+                    seconds = components[0] * 3600 + components[1] * 60 + components[2];
+                }
+                else if (components.size() == 2)
+                {
+                    seconds = components[0] * 60 + components[1];
+                }
+                else
+                {
+                    seconds = components[0];
+                }
+            }
+            else if (argument == "-c")
+            {
+                i++;
+                if (i >= argc)
+                {
+                    throw runtime_error("Cache file name not specified");
+                }
+                cache_file_name = argv[i];
+            }
+            else if (argument == "-h")
+            {
+                show_help = true;
+            }
+            else
+            {
+                throw runtime_error("Argument " + argument + " not recognized");
+            }
         }
-        cout << "Opening " << argv[1] << endl;
-        ifstream input_file(argv[1], ios_base::binary);
+        if (show_help)
+        {
+            cout << "Usage: dec2bin [arguments]" << endl;
+            cout << "Arguments:" << endl;
+            cout << " -i : Path to input decimal number file" << endl;
+            cout << " -o : Path to output binary number file" << endl;
+            cout << " -s : Offset into input file (starting index)" << endl;
+            cout << " -l : Amount of digits to read from file (length)" << endl;
+            cout << " -t : Minimum amount of time that dec2bin will run (hh:mm:ss)" << endl;
+            cout << " -c : Path to cache file storing results for a partial run" << endl;
+            return EXIT_FAILURE;
+        }
+        cout << "Opening " << input_file_name << endl;
+        ifstream input_file(input_file_name, ios_base::binary);
         input_file.seekg(0, ios_base::end);
-        auto file_size = input_file.tellg();
-        if (file_size <= 0)
+        size_t input_file_size = input_file.tellg();
+        if (input_file_size == 0)
         {
-            throw runtime_error(string(argv[1]) + " - empty or absent input file");
+            throw runtime_error(input_file_name + " - empty or absent input file");
         }
-        long offset = 0;
-        if (argc >= 4)
+        if (input_start >= input_file_size)
         {
-            offset = atol(argv[3]);
-            if (offset <= 0)
-            {
-                throw runtime_error("Offset " + string(argv[2]) + " is not a positive number");
-            }
-            if (offset >= file_size)
-            {
-                throw runtime_error("Offset " + string(argv[2]) + " exceeds the size of the input file");
-            }
+            throw runtime_error("Offset " + to_string(input_start) + " exceeds the size of the input file");
         }
-        input_file.seekg(offset);
-        long number_size = (size_t)file_size - offset;
-        if (argc == 5)
+        if (input_start + input_length > input_file_size)
         {
-            number_size = atol(argv[4]);
-            if (number_size <= 0)
-            {
-                throw runtime_error("Amount " + string(argv[3]) + " is not a positive number");
-            }
-            if (number_size + offset >= file_size)
-            {
-                throw runtime_error("Amount " + string(argv[3]) + " plus offset " + string(argv[4]) + " exceeds the size of the input file");
-            }
+            throw runtime_error("Amount " + to_string(input_length) + " plus offset " + to_string(input_start) + " exceeds the size of the input file");
+        }
+        input_file.seekg(input_start);
+        auto number_size = input_length;
+        if (number_size == 0)
+        {
+            number_size = input_file_size - input_start;
         }
         vector<unsigned char> input_buffer(number_size);
-        cout << "Reading " << argv[1] << endl;
+        cout << "Reading " << input_file_name << endl;
         input_file.read((char*)(input_buffer.data()), number_size);
+        if (input_file.fail())
+        {
+            throw runtime_error("Could not read data from " + input_file_name);
+        }
         for (size_t i = 0; i < number_size; i++)
         {
             auto c = input_buffer[i];
             if (c < '0' || c > '9')
             {
-                throw runtime_error("Character " + to_string(i + offset) + " in " + argv[1] + " is not a number (" + to_string(c) + ")");
+                throw runtime_error("Character '" + string(1, c) + "' in " + input_file_name + " is not a number");
             }
             input_buffer[i] = c - '0';
         }
-        cout << "Processing number with " << number_size << " digits" << endl;
-        auto output_buffer_size = (size_t)ceil(number_size / 4);
+        input_file.close();
+        auto output_buffer_size = (size_t)ceil(number_size / sizeof(uint32_t)) + 1;
+        cout << "Allocating " << output_buffer_size * sizeof(uint32_t) << " bytes for output" << endl;
         vector<uint32_t> output_buffer(output_buffer_size);
-        auto start_time = chrono::high_resolution_clock::now();
+        size_t iteration = 0;
         size_t start = output_buffer_size - 1;
-        for (size_t i = 0; i < input_buffer.size(); i++)
+        size_t finish = start;
+        if (cache_file_name.length() > 0)
         {
-            uint64_t carry = input_buffer[i];
-            for (size_t j = output_buffer_size - 1; j >= start; j--)
+            cout << "Opening cache at " << cache_file_name << endl;
+            ifstream cache_file(cache_file_name, ios_base::binary);
+            if (!cache_file.fail())
             {
-                uint64_t value = (uint64_t)output_buffer[j] * 10 + carry;
+                cache_file.seekg(0, ios_base::end);
+                size_t cache_file_size = cache_file.tellg();
+                size_t cache_length = 2 * sizeof(size_t) + output_buffer_size * sizeof(uint32_t);
+                if (cache_file_size != cache_length)
+                {
+                    throw runtime_error("Cache file " + cache_file_name + " is not exactly " + to_string(cache_length) + " bytes in size");
+                }
+                cache_file.seekg(0);
+                cout << "Reading cache file " << cache_file_name << endl;
+                cache_file.read((char*)&iteration, sizeof(size_t));
+                if (cache_file.fail())
+                {
+                    throw runtime_error("Could not read iteration number from cache file " + cache_file_name);
+                }
+                if (iteration >= number_size)
+                {
+                    throw runtime_error("Iteration number " + to_string(iteration) + " in cache file " + cache_file_name + " exceeds the size of the input file");
+                }
+                cache_file.read((char*)&start, sizeof(size_t));
+                if (cache_file.fail())
+                {
+                    throw runtime_error("Could not read binary number start from cache file " + cache_file_name);
+                }
+                if (start > finish)
+                {
+                    throw runtime_error("Binary number start " + to_string(start) + " in cache file " + cache_file_name + " exceeds the size of the output number buffer");
+                }
+                cout << "Iteration number: " << iteration << " of " << number_size << endl;
+                cout << "Binary number start: " << start << " of " << finish << endl;
+                cache_file.read((char*)(output_buffer.data()), output_buffer_size * sizeof(uint32_t));
+                if (cache_file.fail())
+                {
+                    throw runtime_error("Could not read output number data from cache file " + cache_file_name);
+                }
+            }
+        }
+        cout << "Processing number with " << number_size << " digits" << endl;
+        auto start_time = chrono::high_resolution_clock::now();
+        while (iteration < number_size)
+        {
+            uint64_t carry = input_buffer[iteration];
+            for (size_t i = finish; i >= start; i--)
+            {
+                uint64_t value = (uint64_t)output_buffer[i] * 10 + carry;
                 carry = value / 4294967296;
-                output_buffer[j] = (uint32_t)(value % 4294967296);
+                output_buffer[i] = (uint32_t)(value % 4294967296);
             }
             if (carry > 0)
             {
                 start--;
                 output_buffer[start] = (uint32_t)carry;
             }
+            iteration++;
+            if (seconds > 0)
+            {
+                auto iteration_time = chrono::high_resolution_clock::now();
+                chrono::duration<double> elapsed = iteration_time - start_time;
+                if (elapsed.count() > seconds)
+                {
+                    break;
+                }
+            }
         }
         auto finish_time = chrono::high_resolution_clock::now();
         chrono::duration<double> elapsed = finish_time - start_time;
         cout << "Processing time: " << elapsed.count() << " seconds" << endl;
-        cout << "Writing " << argv[2] << endl;
-        ofstream output_file(argv[2], ios_base::binary);
-        size_t shift = 24;
-        if (output_buffer[start] < 256)
+        if (iteration < number_size)
         {
-            shift = 0;
-        }
-        else if (output_buffer[start] < 65536)
-        {
-            shift = 8;
-        }
-        else if (output_buffer[start] < 16777216)
-        {
-            shift = 16;
-        }
-        while (start < output_buffer_size)
-        {
-            output_file.put((output_buffer[start] >> shift) & 255);
-            if (shift == 0)
+            cout << "Iteration number: " << iteration << " of " << number_size << endl;
+            cout << "Binary number start: " << start << " of " << finish << endl;
+            cout << "Writing cache file " << cache_file_name << endl;
+            ofstream cache_file(cache_file_name, ios_base::binary);
+            if (cache_file.fail())
             {
-                start++;
-                shift = 24;
+                throw runtime_error("Could not open cache file " + cache_file_name);
             }
-            else
+            cache_file.write((char*)&iteration, sizeof(size_t));
+            if (cache_file.fail())
             {
-                shift -= 8;
+                throw runtime_error("Could not write iteration number to cache file " + cache_file_name);
             }
+            cache_file.write((char*)&start, sizeof(size_t));
+            if (cache_file.fail())
+            {
+                throw runtime_error("Could not write binary number start to cache file " + cache_file_name);
+            }
+            cache_file.write((char*)(output_buffer.data()), output_buffer_size * sizeof(uint32_t));
+            cache_file.close();
+            cout << "Cache file " << cache_file_name << " written" << endl;
         }
-        output_file.close();
+        else
+        {
+            cout << "Writing " << output_file_name << endl;
+            ofstream output_file(output_file_name, ios_base::binary);
+            size_t shift = 24;
+            if (output_buffer[start] < 256)
+            {
+                shift = 0;
+            }
+            else if (output_buffer[start] < 65536)
+            {
+                shift = 8;
+            }
+            else if (output_buffer[start] < 16777216)
+            {
+                shift = 16;
+            }
+            while (start < output_buffer_size)
+            {
+                output_file.put((output_buffer[start] >> shift) & 255);
+                if (shift == 0)
+                {
+                    start++;
+                    shift = 24;
+                }
+                else
+                {
+                    shift -= 8;
+                }
+            }
+            output_file.close();
+            cout << output_file_name << " written"  << endl;;
+        }
+        return EXIT_SUCCESS;
     }
     catch (exception& e)
     {
         cout << "Error: " << e.what() << endl;
+        return EXIT_FAILURE;
     }
 }
